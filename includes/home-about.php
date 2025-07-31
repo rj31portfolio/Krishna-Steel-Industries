a4fddc391dc0b06a7c3934306bd32dc83f6db043
rj31portfolio <rjrajuali31@gmail.com>
2025-07-31 13:09:11 +0530

    change images in about

--- includes/home-about.php
+++ includes/home-about.php
@@ -12,7 +12,7 @@
           </div>
           <div class="about-sec1 img2">
             <div class="about-sec1-inner">
-              <img src="<?php echo $base_url; ?>images/about/02.png" class="img-fluid" alt="">
+              <img src="<?php echo $base_url; ?>images/about/about-2.png" class="img-fluid" alt="">
             </div>
             <div class="round-text">
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 180 180" enable-background="new 0 0 180 180" xml:space="preserve">
